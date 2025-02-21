ae6733f5af34ae055e3aead3292af662e93c3912
gexu <gexu@singhand.com>
2025-02-21 18:41:08 +0800

    update: 使用Cypher DSL查询实体

--- biz-graph/src/main/java/com/singhand/sr/graphservice/bizgraph/service/impl/VertexServiceImpl.java
+++ biz-graph/src/main/java/com/singhand/sr/graphservice/bizgraph/service/impl/VertexServiceImpl.java
@@ -1,12 +1,22 @@
 package com.singhand.sr.graphservice.bizgraph.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import com.singhand.sr.graphservice.bizgraph.model.NewVertexRequest;
 import com.singhand.sr.graphservice.bizgraph.service.VertexService;
 import com.singhand.sr.graphservice.bizmodel.model.neo4j.Vertex;
-import com.singhand.sr.graphservice.bizmodel.repository.neo4j.*;
+import com.singhand.sr.graphservice.bizmodel.repository.neo4j.EdgeRepository;
+import com.singhand.sr.graphservice.bizmodel.repository.neo4j.FeatureRepository;
+import com.singhand.sr.graphservice.bizmodel.repository.neo4j.PropertyRepository;
+import com.singhand.sr.graphservice.bizmodel.repository.neo4j.PropertyValueRepository;
+import com.singhand.sr.graphservice.bizmodel.repository.neo4j.VertexRepository;
 import jakarta.annotation.Nonnull;
+import java.util.Map;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
+import org.neo4j.cypherdsl.core.Condition;
 import org.neo4j.cypherdsl.core.Cypher;
+import org.neo4j.cypherdsl.core.Expression;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,9 +24,6 @@ import org.springframework.data.neo4j.core.Neo4jTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Map;
-import java.util.Set;
 
 @Service
 @Slf4j
@@ -80,6 +87,19 @@ public class VertexServiceImpl implements VertexService {
     final var name = vertex.property("name");
     final var type = vertex.property("type");
 
-    return null;
+    Condition condition = Cypher.noCondition();
+    if (StrUtil.isNotBlank(keyword)) {
+      condition = name.contains(Cypher.literalOf(keyword));
+    }
+
+    if (CollUtil.isNotEmpty(types)) {
+      Expression namesList = Cypher.listOf(types.stream()
+          .map(Cypher::literalOf)
+          .toArray(Expression[]::new));
+      final var typeIn = type.in(namesList);
+      condition = condition.and(typeIn);
+    }
+
+    return vertexRepository.findAll(condition, pageable);
   }
 }
