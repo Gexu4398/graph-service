57ed912fe7c173c504d5a4668d52e716e73b7c44
gexu <gexu@singhand.com>
2025-03-11 10:21:55 +0800

    feat(biz-model): 使用 Neo4j 替代 Elasticsearch 作为向量存储
    - 在 application.yml 中添加了 Neo4j 向量存储的配置
    - 新增 Neo4jVectorStoreConfig 类用于配置 Neo4j 向量存储- 更新 pom.xml，添加 Neo4j 存储和 OpenAI相关依赖
    - 移除了 Elasticsearch 相关的注释配置

---
+++ biz-model/src/main/java/com/singhand/sr/graphservice/bizmodel/datasource/Neo4jVectorStoreConfig.java
@@ -0,0 +1,60 @@
+package com.singhand.sr.graphservice.bizmodel.datasource;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.neo4j.driver.Driver;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.embedding.TokenCountBatchingStrategy;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.vectorstore.neo4j.Neo4jVectorStore;
+import org.springframework.ai.vectorstore.neo4j.Neo4jVectorStore.Neo4jDistanceType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.ai.vectorstore.neo4j")
+@Setter
+@Getter
+public class Neo4jVectorStoreConfig {
+
+  private String databaseName;
+
+  private Neo4jDistanceType distanceType;
+
+  private int embeddingDimension;
+
+  private String indexName;
+
+  private boolean initializeSchema;
+
+  @Value("${spring.ai.openai.api-key}")
+  private String apiKey;
+
+  @Value("${spring.ai.openai.base-url}")
+  private String baseUrl;
+
+  @Bean
+  public Neo4jVectorStore neo4jVectorStore(Driver neo4jDriver, EmbeddingModel embeddingModel) {
+
+    return Neo4jVectorStore.builder(neo4jDriver, embeddingModel)
+        .databaseName(databaseName)
+        .distanceType(distanceType)
+        .embeddingDimension(embeddingDimension)
+        .indexName(indexName)
+        .initializeSchema(initializeSchema)
+        .batchingStrategy(new TokenCountBatchingStrategy())
+        .build();
+  }
+
+  @Bean
+  public EmbeddingModel embeddingModel() {
+
+    return new OpenAiEmbeddingModel(OpenAiApi.builder()
+        .apiKey(apiKey)
+        .baseUrl(baseUrl)
+        .build());
+  }
+}
