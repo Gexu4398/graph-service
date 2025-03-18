b4cb72313e9b4e62cca566f0ada154f5fd9d81ca
gexu <gexu@singhand.com>
2025-03-18 17:19:30 +0800

    refactor(biz-keycloak-model): 将 LocaleHelper 类声明为 final 类
    - 将 LocaleHelper 类的修饰符从 public 改为 public final
    - 此修改旨在确保该类的实例和方法不会被外部类继承或修改

--- biz-keycloak-model/src/main/java/com/singhand/sr/graphservice/bizkeycloakmodel/helper/LocaleHelper.java
+++ biz-keycloak-model/src/main/java/com/singhand/sr/graphservice/bizkeycloakmodel/helper/LocaleHelper.java
@@ -3,7 +3,7 @@ package com.singhand.sr.graphservice.bizkeycloakmodel.helper;
 /**
  * 语言环境帮助类
  */
-public class LocaleHelper {
+public final class LocaleHelper {
 
   public static final String ZH_CN = "zh-CN";
 
