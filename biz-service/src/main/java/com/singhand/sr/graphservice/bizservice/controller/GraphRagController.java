1f6a3ba57100b4a3b910cedb962854f1ae84b6f9
gexu <gexu@singhand.com>
2025-03-19 16:16:43 +0800

    refactor(biz-service): 将 GraphRagController 中的 queryRag 方法从 GET 映射改为 POST 映射
    - 将 queryRag 方法的注解从 @GetMapping 改为 @PostMapping- 这个改动可以更好地处理复杂的请求体，提高系统的可用性和扩展性

--- biz-service/src/main/java/com/singhand/sr/graphservice/bizservice/controller/GraphRagController.java
+++ biz-service/src/main/java/com/singhand/sr/graphservice/bizservice/controller/GraphRagController.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +33,7 @@ public class GraphRagController {
    * @return 查询结果
    */
   @Operation(summary = "通过RAG查询")
-  @GetMapping
+  @PostMapping
   public String queryRag(@Valid @RequestBody RagRequest ragRequest) {
 
     return graphRagService.query(ragRequest);
